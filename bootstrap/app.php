6f9595cde78c13f0c455baecc061c5201c4a59b5
Mike <michael.visan@rightglobalgroup.com>
2026-04-10 12:05:31 +0100

    Exclude hub API routes from CSRF verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
             'filament.auth' => \App\Http\Middleware\FilamentAuth::class,
         ]);
 
+        $middleware->validateCsrfTokens(except: [
+            'api/fees/report',
+            'api/messages',
+        ]);
+
         //
     })
     ->withProviders([
