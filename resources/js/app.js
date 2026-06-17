7dc08ffd135dba1a2450ad923fb58098d4d1e7e8
max-behrens-rgg <max.behrens@rightglobalgroup.com>
2026-06-17 11:24:03 +0100

    Inetia ssr test

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
+import { createSSRApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { SiteTextPlugin } from './Plugins/siteText';
 
@@ -17,7 +17,7 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        return createSSRApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .use(SiteTextPlugin)
